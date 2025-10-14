a5a25cb57faea4f702fb50ae817355eedfa3b59f
bpauquette <bryan.pauquette@gmail.com>
2025-10-14 16:18:08 -0400

    chore: eslint --fix applied (formatting)

---
+++ src/colorSchemes.js
@@ -0,0 +1,29 @@
+// colorSchemes.js
+export const colorSchemes = {
+  neon: {
+    name: "Neon Grid",
+    background: "#0a0a1a",
+    getCellColor: (x, y) => `hsl(${(x * 50 + y * 90) % 360}, 100%, 60%)`
+  },
+  bio: {
+    name: "BioLife",
+    background: "#0e1a12",
+    getCellColor: () => "#4aff6c"
+  },
+  ember: {
+    name: "Ember Field",
+    background: "#1a0b0b",
+    getCellColor: (x, y) => `hsl(${20 + ((x + y) % 20)}, 90%, 50%)`
+  },
+  retro: {
+    name: "Retro Vector",
+    background: "#001f3f",
+    getCellColor: () => "#39ff14"
+  },
+  spectrum: {
+    name: "Spectrum Pulse",
+    background: "#000",
+    getCellColor: (x, y, t = Date.now()) => 
+      `hsl(${(t / 20 + x * 10 + y * 5) % 360}, 100%, 50%)`
+  }
+};
