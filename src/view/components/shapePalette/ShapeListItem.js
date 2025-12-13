f9ee9ef844d7ed265eb3852b0ca6426cf4be8894
bpauquette <bryan.pauquette@gmail.com>
2025-12-13 14:47:11 -0500

    Fix duplicate import

--- src/view/components/shapePalette/ShapeListItem.js
+++ src/view/components/shapePalette/ShapeListItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import ListItem from '@mui/material/ListItem';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
+// ...existing code...
 import ListItemButton from '@mui/material/ListItemButton';
 import Typography from '@mui/material/Typography';
 import { Delete as DeleteIcon, Public as PublicIcon, PublicOff as PublicOffIcon } from '@mui/icons-material';
